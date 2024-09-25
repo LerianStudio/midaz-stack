601f24a655f1234f095b68cf8c72f2e6c366c3a8
MartinezAvellan <martinez.avellan@hotmail.com>
2024-09-25 19:17:04 +0200

    fix: update make.sh

--- make.sh
+++ make.sh
@@ -4,20 +4,28 @@ set -e
 
 git submodule update --init --recursive --checkout
 
-cd midaz/components/auth
+cd midaz
+
+cd components
+
+cd ledger
 mv .env.example .env
 
-cd ../ledger
-mv .env.example .env
+cd ..
 
-cd ../transaction
+cd transaction
 mv .env.exemple .env
 
-cd ../../
+cd ..
+
+cd ..
+
 make ledgers COMMAND="up"
 make transaction COMMAND="up"
 
-cd ../midaz-console
+cd ..
+
+cd midaz-console
 npm run dev
 
 docker-compose up -d
